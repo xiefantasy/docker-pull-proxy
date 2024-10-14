a39834bc59e14ebae834b3d4929cb2f81af2801d
xiedh <294688426@qq.com>
2024-10-14 17:02:36 +0800

    redis

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 #nginx:alpine registry.cn-hongkong.aliyuncs.com/xiefantasy/nginx:alpine
 gogs/gogs:latest registry.cn-hongkong.aliyuncs.com/xiefantasy/gogs:latest
 mysql:8.0 registry.cn-hongkong.aliyuncs.com/xiefantasy/mysql:8.0
+redis:7.4 registry.cn-hongkong.aliyuncs.com/xiefantasy/redis:7.4
