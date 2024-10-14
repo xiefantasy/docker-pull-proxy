7890a0a74bd579b47875acc754e6510666be5b97
xiedh <294688426@qq.com>
2024-10-14 17:06:03 +0800

    redis

--- test.py
+++ test.py
@@ -2,3 +2,5 @@
 gogs/gogs:latest registry.cn-hongkong.aliyuncs.com/xiefantasy/gogs:latest
 mysql:8.0 registry.cn-hongkong.aliyuncs.com/xiefantasy/mysql:8.0
 redis:7.4 registry.cn-hongkong.aliyuncs.com/xiefantasy/redis:7.4
+mongo:8.0 registry.cn-hongkong.aliyuncs.com/xiefantasy/mongo:8.0
+jenkins/jenkins:latest registry.cn-hongkong.aliyuncs.com/xiefantasy/jenkins:latest
