c582de71ae7b198f3a31409fa5ea805653732927
xiedh <294688426@qq.com>
2024-10-14 16:52:50 +0800

    gogs

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 #nginx:alpine registry.cn-hongkong.aliyuncs.com/xiefantasy/nginx:alpine
 gogs/gogs:latest registry.cn-hongkong.aliyuncs.com/xiefantasy/gogs:latest
+mysql:8.0 registry.cn-hongkong.aliyuncs.com/xiefantasy/mysql:8.0
