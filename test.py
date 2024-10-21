e87fa5d5db8998e2b9dd51f4de2a77106343f0c7
xiedh <294688426@qq.com>
2024-10-21 17:01:09 +0800

    python

--- test.py
+++ test.py
@@ -5,3 +5,4 @@ redis:7.4 registry.cn-hongkong.aliyuncs.com/xiefantasy/redis:7.4
 mongo:8.0 registry.cn-hongkong.aliyuncs.com/xiefantasy/mongo:8.0
 jenkins/jenkins:latest registry.cn-hongkong.aliyuncs.com/xiefantasy/jenkins:latest
 python:3.12.2-slim registry.cn-shanghai.aliyuncs.com/xiefantasy/python:3.12.2-slim
+python:3.13.0-slim registry.cn-shanghai.aliyuncs.com/xiefantasy/python:3.13.0-slim
