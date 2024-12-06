8e144ae83877058df4d17c79d653827a079c6b5f
xiedh <294688426@qq.com>
2024-12-06 16:59:22 +0800

    python

--- test.py
+++ test.py
@@ -11,3 +11,4 @@ swarmpit/swarmpit:latest registry.cn-shanghai.aliyuncs.com/xiefantasy/swarmpit:l
 couchdb:2.3.0 registry.cn-shanghai.aliyuncs.com/xiefantasy/couchdb:2.3.0
 influxdb:1.8 registry.cn-shanghai.aliyuncs.com/xiefantasy/influxdb:1.8
 swarmpit/agent:latest registry.cn-shanghai.aliyuncs.com/xiefantasy/swarmpit-agent:latest
+registry:2 registry.cn-shanghai.aliyuncs.com/xiefantasy/registry:2
