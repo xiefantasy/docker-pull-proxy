3d358aa29a7807a9b9fbe8139646e42886158d7e
xiedh <294688426@qq.com>
2026-06-26 16:18:24 +0800

    rust

--- test.py
+++ test.py
@@ -17,3 +17,4 @@ python:3.14.0-slim registry.cn-shanghai.aliyuncs.com/xiefantasy/python:3.14.0-sl
 python:3.14.2-slim registry.cn-shanghai.aliyuncs.com/xiefantasy/python:3.14.2-slim
 nginx:latest registry.cn-shanghai.aliyuncs.com/xiefantasy/nginx:latest
 rust:1.96-slim-bookworm registry.cn-shanghai.aliyuncs.com/xiefantasy/rust:1.96-slim-bookworm
+debian:bookworm-slim registry.cn-shanghai.aliyuncs.com/xiefantasy/debian:bookworm-slim
